05fe1441c07a4af638973f85ca0ad5e4000872ed
hoochicken <mareike.riegel@ql.de>
2024-07-20 21:11:10 +0200

    version 1 with text

---
+++ src/components/general/Technique.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="align-middle name">{{technique.name}}</div>
+    <img v-if="technique.image" src="technique.image" alt="technique.name"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Technique",
+  props: ['technique'],
+  data: () => ( {
+    isLoading: false
+  }),
+  mounted: function() {
+    // this.initiateTechnique();
+  },
+  methods: {
+    async initiateTechnique() {
+      this.isLoading = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.name {font-size:1.5rem;text-align: center;color:white;background: #282828;}
+</style>
